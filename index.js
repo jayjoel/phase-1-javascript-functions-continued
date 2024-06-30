7c2c957a734dbc95f81a6476ffb94292afc111a3
joel_patrick <jayjoelpn@gmail.com>
2024-06-30 15:25:17 +0300

    functions cont

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// Function to express Saturday activities
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Function to express Monday activities
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+// Function to wrap an adjective in a highlight symbol
+function wrapAdjective(symbol = '*') {
+    return function(adjective = 'special') {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
+
+
